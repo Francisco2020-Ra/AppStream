271180d1a620bc9dcd5618f24c9dc77a6774c72b
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2021-11-08 21:35:41 -0300

    AppStream

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Stream<String> streamVacio = Stream.empty();
+        System.out.println(streamVacio);
+    }
+}
